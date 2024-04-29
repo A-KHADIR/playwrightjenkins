b6e6e11a6594a7736f0378b5c1481225011c4655
chamsedine <braikchamsedine@gmail.com>
2024-04-29 11:08:34 +0200

    gggg

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,26 +1,26 @@
-// @ts-check
-import { MainPage } from "../tests/pomm";
-const donnees = require('..//données/données.json');
+// // @ts-check
+// import { MainPage } from "../tests/pomm";
+// const donnees = require('..//données/données.json');
 
-const { test, expect } = require('@playwright/test');
-test('has title', async ({ page}) => {
-for( const donnee of donnees.recherches){ 
-       const mainPage = new MainPage(page);
+// const { test, expect } = require('@playwright/test');
+// test('has title', async ({ page}) => {
+// for( const donnee of donnees.recherches){ 
+//        const mainPage = new MainPage(page);
 
 
-  await mainPage.goToHomePage();
-  await page.locator("//textarea[@id='APjFqb']").fill(donnee);
-  await page.locator("(//input[@value='Recherche Google'])[1]").click();
+//   await mainPage.goToHomePage();
+//   await page.locator("//textarea[@id='APjFqb']").fill(donnee);
+//   await page.locator("(//input[@value='Recherche Google'])[1]").click();
   
-  await page.waitForSelector('body', { state: 'attached' });
+//   await page.waitForSelector('body', { state: 'attached' });
 
-  const pageTitle = await page.title();
-    console.log(`Terme de recherche : ${donnee}, Titre de la page : ${pageTitle}`);
-    await page.waitForTimeout(2000);
-}
+//   const pageTitle = await page.title();
+//     console.log(`Terme de recherche : ${donnee}, Titre de la page : ${pageTitle}`);
+//     await page.waitForTimeout(2000);
+// }
 
-await page.close();
-console.log(`Fin du test`);
+// await page.close();
+// console.log(`Fin du test`);
 
-});
-//730.63
+// });
+// //730.63
--- tests/testenParallele.spec.js
+++ tests/testenParallele.spec.js
@@ -1,27 +1,27 @@
 
-// j'ai ajputé ce commentaire afin de test le push via vscod directement 
+// j'ai ajouté ce commentaire afin de test le push via vscod directement 
 
-// // @ts-check
-// import { MainPage } from "../tests/pomm";
-// const donnees = require('..//données/données.json');
+// @ts-check
+import { MainPage } from "../tests/pomm";
+const donnees = require('..//données/données.json');
 
 
-// const { test, expect } = require('@playwright/test');
-// test('has title', async ({ browser}) => {
-// const promises = donnees.recherches.map( async(donnee) =>{
-//        const page = await browser.newPage();
-//        const mainPage = new MainPage(page);
+const { test, expect } = require('@playwright/test');
+test('has title', async ({ browser}) => {
+const promises = donnees.recherches.map( async(donnee) =>{
+       const page = await browser.newPage();
+       const mainPage = new MainPage(page);
 
-//   await mainPage.goToHomePage();
-//   await page.locator("//textarea[@id='APjFqb']").fill(donnee);
-//   await page.locator("(//input[@value='Recherche Google'])[1]").click();
-//   await page.waitForSelector('body', { state: 'attached' });
+  await mainPage.goToHomePage();
+  await page.locator("//textarea[@id='APjFqb']").fill(donnee);
+  await page.locator("(//input[@value='Recherche Google'])[1]").click();
+  await page.waitForSelector('body', { state: 'attached' });
 
-//   const pageTitle = await page.title();
-//     console.log(`Terme de recherche : ${donnee}, Titre de la page : ${pageTitle}`);
-//     await page.waitForTimeout(2000);
-//
+  const pageTitle = await page.title();
+    console.log(`Terme de recherche : ${donnee}, Titre de la page : ${pageTitle}`);
+    await page.waitForTimeout(2000);
 
-// await page.close()});
-// await Promise.all(promises);
-// });
+
+await page.close()});
+await Promise.all(promises);
+});
